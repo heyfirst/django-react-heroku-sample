32546a536ecf2f60c51dc68851ddbe454d6d01de
Kanisorn Sutham <firstziiz.k@gmail.com>
2021-02-20 13:45:55 +0700

    add create local superuser

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,10 @@ cd backend
 echo "Apply database migrations"
 poetry run python manage.py migrate
 
+# create local superuser
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | poetry run python manage.py shell
+
 # Start server
 echo "Starting server"
 poetry run python manage.py runserver 0.0.0.0:8000
+
